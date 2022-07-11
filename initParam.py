768e467e1d59518a5a9efd0c7b534f43dbbf6fb3
fu <kevinsun17@126.com>
2022-07-11 17:26:48 +0800

    comment in initParam

--- initParam.py
+++ initParam.py
@@ -1,12 +1,15 @@
+## video or frame source
 link = './pianoSound/hand6.mp4' #from video
 # link=0 #from camera 0
 
 ## showup window size
 wSize = 800
 hSize = 450
+
 ## area range for Thumb, index finger, middle finger, ring finger, little finger
 rangeIndexList = [0.4,0.4,0.4,0.4,0.4]
-maxLen = 5000
 
 ## whether skip some frame,if ==1 do not skip
 skipFrmae = 2
+
+maxLen = 5000
